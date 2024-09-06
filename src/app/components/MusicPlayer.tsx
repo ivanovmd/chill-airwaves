6616a6938e07ebab650b54d108f27ead8f02a361
Sergey <iserg.junk@gmail.com>
2024-09-05 21:15:00 -0400

    added track info fetching

--- src/app/components/MusicPlayer.tsx
+++ src/app/components/MusicPlayer.tsx
@@ -1,6 +1,37 @@
 import React, { useEffect } from "react"
 import { SceneLayer } from "./Scene";
 
+interface VideoInfo {
+  title: string;
+  author_name: string;
+  author_url: string;
+  thumbnail_url: string;
+}
+
+function extractYouTubeVideoId(url: string): string | null {
+  const regex = /(?:v=|\/)([0-9A-Za-z_-]{11}).*$/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
+}
+
+const fetchVideoInfo = async (url: any) => {
+  try {
+    const response = await fetch(
+      `https://www.youtube.com/oembed?url=${encodeURIComponent(url)}&format=json`
+    );
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch video info');
+    }
+
+    const data: VideoInfo = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log('Error fetching video info. Make sure the URL is correct and the video exists.');
+    console.error(err);
+  }
+};
 
 const loadYoutubeIrfameAPI = (): Promise<void> => {
   return new Promise((resolve, reject) => {
@@ -56,15 +87,20 @@ const createYoutubePlayer = (onPlayerReady: CallableFunction, onPlayerStateChang
 
 export const MusicPlayer = () => {
   const [youtubePlayer, setYoutubePlayer] = React.useState<any>(null);
+  const [videoInfo, setVideoInfo] = React.useState<VideoInfo | null>(null);
 
   const onPlayerReady = (event: any) => {
     event.target.playVideo();
   }
 
-  const onPlayerStateChange = (event: any) => {
-
+  async function onPlayerStateChange(event: any) {
+    // https://www.youtube.com/watch?list=RDCLAK5uy_nZJzoZEBYRptA2XXskbxGTvKkevapT_F4&v=qGVMBirxwWc
     if (event.data === -1) {
-      console.log(youtubePlayer?.getVideoUrl())
+      const curretUrl = youtubePlayer.getVideoUrl()
+      const videoId = extractYouTubeVideoId(curretUrl)
+      console.log(videoId);
+      const videoInfo = await fetchVideoInfo(curretUrl)
+      setVideoInfo(videoInfo);
     }
   }
 
@@ -107,6 +143,18 @@ export const MusicPlayer = () => {
       </div>
       <div>
         <input type="range" min="0" max="100" onChange={(e) => setVolume(parseInt(e.target.value))} />
+      </div>
+
+      <div>
+        {videoInfo && (
+          <div>
+            <div className="h-20 w-20 relative overflow-hidden rounded-full">
+              <img style={{ top: '-18%', height: '135%' }} className="absolute inset-0 w-full h-full object-cover object-center pointer-events-none" src={videoInfo.thumbnail_url} alt="" />
+            </div>
+            <p>{videoInfo.title}</p>
+            <p>{videoInfo.author_name}</p>
+          </div>
+        )}
       </div>
     </SceneLayer>
   )
