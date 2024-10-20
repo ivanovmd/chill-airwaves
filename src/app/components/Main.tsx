b838ab41aafa8dee08ca09ccaabf9367ff6c95e8
Serghei Ivanov <ivanovmd1990@gmail.com>
2024-10-20 18:51:52 -0400

    added splash screen

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -4,10 +4,10 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "react-redux";
 import { persistor, store } from "../store";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Toaster } from "react-hot-toast";
 import { Player } from "./screens/Player";
-import { AppLoader } from "./screens/AppLoader";
+import { Splashscreen } from "./screens/Splashscreen";
 import { Login } from "./screens/Login";
 import { ModalProvider } from "../context/ModalContext";
 import { PersistGate } from "redux-persist/integration/react";
@@ -25,8 +25,22 @@ export const Main: FC = () => {
 
     setTimeout(() => {
       setIsInitialLoading(false);
-    }, 2000);
+    }, 4000);
   }, []);
+
+
+  //if (isInitialLoading) {
+  //  return <AnimatePresence>
+  //    <motion.div
+  //      initial={{ opacity: 0 }}
+  //      animate={{ opacity: 1 }}
+  //      exit={{ opacity: 0 }}
+  //      transition={{ duration: 2 }}
+  //    >
+  //      <Splashscreen fadeOut={!isInitialLoading} />
+  //    </motion.div>
+  //  </AnimatePresence >;
+  //}
 
 
   return (
@@ -38,10 +52,10 @@ export const Main: FC = () => {
             <ModalProvider>
               <BrowserRouter>
                 <AnimatePresence mode="wait">
-                  <Routes>
+                  {isInitialLoading ? <Splashscreen fadeOut={false} /> : <Routes>
                     <Route path="/" element={<Player />} />
                     <Route path="/auth" element={<Login />} />
-                  </Routes>
+                  </Routes>}
                 </AnimatePresence>
               </BrowserRouter>
 
